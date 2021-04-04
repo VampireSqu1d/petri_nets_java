6b263f2b7cfdb36128eb9ce8deef8530d924b627
VampireSqu1d <vampiresqu1d@tutanota.com>
2021-04-04 15:27:20 -0700

    segundo commit, ya se imprime M0, y se calcula e imprime M1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,14 +11,16 @@ public class Main {
         *  T3  0
         *  T4  1
         */
-        int[][] pos = {
+        final int[][] pos = {
+                // esta es una matriz 5x5 se pueden  cambiar de acuerdo a la red de petri
                 {1, 0, 1, 0, 0},
                 {0, 1, 0, 0, 0},
                 {1, 0, 0, 0, 0},
                 {1, 0, 0, 0, 1},
                 {0, 0, 0, 1, 0} };
 
-        int[][] pre = {
+        final int[][] pre = {
+                // esta es una matriz 5x5 se pueden  cambiar de acuerdo a la red de petri
                 {0, 1, 0, 0, 0},
                 {1, 0, 1, 0, 0},
                 {0, 0, 0, 1, 0},
@@ -26,64 +28,95 @@ public class Main {
                 {1, 0, 0, 0, 1} };
 
         // se inicializa la matriz de incidencia con el mismo largo de la matriz pos
-        int[][] incidence = new int[pos.length][pos.length];
-        // en este loop se hacen las resta de la matriz pre sobre la matriz pos C(t) = pos - pre y se agregan a la matriz incidence
+        int[][] incidence = new int[pos.length][pos[0].length];
+        // en este loop se hacen las resta de la matriz pre sobre la matriz pos C(t) = pos - pre, y se agregan a la matriz incidence
         for (int i = 0; i < pos.length; i++) {
-            for (int j = 0; j < pos.length; j++) {
+            for (int j = 0; j < pos[i].length; j++) {
                 incidence[i][j] = pos[i][j] - pre[i][j];
             }
         }
-
+        // aqui se imprimen las matrices 2x2
         imprimirMatriz(pos, "matriz pos");
         imprimirMatriz(pre, "matriz pre");
         imprimirMatriz(incidence, "matriz de incidencia");
 
-        // cada celda representa la transicion que se puede disparar en el modelo
-        int[] vectorE = {0, 0, 1, 0, 0};
-        // este vector es la multiplicacion del vectorE por la matriz de incidencia
-        int[] vectorCxE = new int[incidence.length];
-        // en este loop se hace la multiplicacion y suma del vector CxE  cada celda del vectorE se multiplica con el vector la columna de la matriz de incidencia
-        for (int i = 0; i < incidence.length; i++) {
-            int CxE = 0;
-            for (int j = 0;  j < incidence[i].length ; j++) {
-                CxE = CxE +  (incidence[j][i]  * vectorE[j]) ;
-            }
-            vectorCxE[i] = CxE;
-        }
-        // aquí se imprimen los arreglos de una dimensión
-        imprimirVector(vectorE, "Vector E");
-        imprimirVector(vectorCxE, "Vector CxE");
-
-        // cada celda representa el número de tokens en cada place
-        // se le conoce como marca inicial y se denomina M0
-        int[] M0 = {1, 0, 1, 0, 1};
-        int[] M1 = new int[M0.length];
-        for (int i = 0; i < M0.length; i++) {
-            M1[i] = M0[i] + vectorCxE[i];
-        }
+        int[] M0 = {1, 0, 1, 0, 1}; // cada celda representa el número de tokens en cada place
         imprimirVector(M0, "vector M0");
-        imprimirVector(M1, "vector M1");
-        //int ciclos = 10;
+
+        int[] tpd = new int[incidence.length]; // este vector representa las Transiciones que se Pueden Disparar
+        final int ciclos = 1;
+        int ciclo = 0;
+        while (ciclo < ciclos) {
+            // en este loop se revisan las transiciones disponibles para dispararse
+            for (int i = 0; i < incidence.length; i++) {
+                int pre_j = 0;
+                for (int j = 0; j < incidence[0].length; j++) {
+                    if (incidence[i][j] > 0) {
+                        if (M0[j] >= incidence[i][j]) pre_j = 1;
+                        else {
+                            pre_j = 0;
+                            break;
+                        }
+                    }
+                }
+                if (pre_j > 0) tpd[i] = 1;
+                else tpd[i] = 0;
+            }
+            imprimirVector(tpd, "Transiciones que se pueden disparar");
+
+
+
+
+
+
+
+
+
+
+
+
+
+            /* int[] vectorCxE = new int[pos[0].length]; // este vector es la multiplicacion del vectorE por la matriz de incidencia
+            for (int i = 0; i < pos[0].length; i++) {
+                // en este loop se hace la multiplicacion y suma del vector CxE
+                // cada celda del vectorE se multiplica con la celda correspondiente de la columna de la matriz de incidencia
+                int CxE = 0;
+                for (int j = 0; j < incidence[i].length; j++) {
+                    CxE = CxE + (incidence[j][i] * tpd[j]);
+                }
+                vectorCxE[i] = CxE;
+            }
+            imprimirVector(vectorCxE, "Vector CxE");*/
+
+            // en este loop se calcula M1 = M0 + Cxe1 y se guarda en el vector M0
+            /* for (int i = 0; i < M0.length; i++) {
+                M0[i] = M0[i] + vectorCxE[i];
+                if (M0[i] < 0) {
+                    M0[i] = 0;
+                }
+            }*/
+            //imprimirVector(M0, "vector M1");
+            ciclo++;
+        }
     }
 
     // método que imprime cada celda de un arreglo de un a dimensión
     private static void imprimirVector(int[] vector, String nombreVector){
-        System.out.println(nombreVector);
+        System.out.printf("%s: ",nombreVector);
         for (int j : vector) {
             System.out.printf("%d ", j);
         }
         System.out.print("\n");
     }
-    // método que imprime cada celda de la matriz
+    // método que imprime cada celda de una matriz de dos dimensiones
     private static void imprimirMatriz(int[][] matriz, String nombreMatriz) {
         System.out.println(nombreMatriz);
         for (int[] ints : matriz) {
-            for (int j = 0; j < matriz.length; j++) {
-                System.out.printf("%d ", ints[j]);
+            for (int anInt : ints) {
+                System.out.printf("%d ", anInt);
             }
             System.out.print("\n");
         }
         System.out.print("\n");
     }
-
 }
